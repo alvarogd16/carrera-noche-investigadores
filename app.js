347ebf53da27b1e584c5a311833cfbcac6a7f575
alvarogd1610 <alvarogarcia16102000@gmail.com>
2021-09-19 22:59:24 +0200

    Añadido el control para el final de carrera y enventos falsos para cuando no haya raspi

--- app.js
+++ app.js
@@ -17,9 +17,26 @@ if(isPi()) {
    
   const Gpio = require('pigpio').Gpio;
 
+	// Motor init
   const motor = new Gpio(17, {mode: Gpio.OUTPUT});
   const servoPulse = { 'RESET': 500, 'START': 2500 };
 
+	// Fin de carrera init
+	// Estan en orden de izquierda a derecha. Ej. pin4 para el más a la izquierda
+	const finCarreraConfig = { mode: Gpio.INPUT,
+															pullUpDown: Gpio.PUD_UP,
+															alert: true};
+	const finCarrera = [new Gpio(26, finCarreraConfig),
+											new Gpio(19, finCarreraConfig),
+											new Gpio(13, finCarreraConfig),
+											new Gpio(6, finCarreraConfig)];
+
+	// Para el debounce de los finales de carrera. El numero son los microsegundos que tiene que 
+	// estar el valor para que se notifique la interrupción con alert.
+	finCarrera.forEach(fC => fC.glitchFilter(10000));
+
+	
+	// Socket logic
   io.on('connection', (socket) => {
     console.log("Socket establish");
     
@@ -35,9 +52,36 @@ if(isPi()) {
 
 			motor.servoWrite(servoPulse.RESET);
 		});
+
+		// Crea los eventos para todos los finales de carrera
+		finCarrera.forEach((fC, index) => {
+			fC.on('alert', (level, tick) => {
+				if(level === 0) {
+					console.log("Fin carrera " + index + " con nivel " + level);
+					socket.emit("meta", index);
+				}
+			})
+		});
+
 	});
+
 } else {
   console.log("Not running in a Raspberry pi");
+
+	// Para cuando se pruebe en un ordenador que no sea una raspberry
+	io.on('connection', (socket) => {
+		console.log("Socket establish");
+
+		socket.on("start", () => console.log("Race start. Without servo :("));
+		socket.on("reset", () => console.log("Race reset. Without servo :("));
+
+		// Evento para pruebas. El cliente lo envia y responde el servidor como si fuera el 
+		// final de carrera.
+		socket.on("falsaMeta", (data) => {
+			console.log("Falso fin carrera " + data);
+			soket.emit("meta", data);
+		});
+	});
 }
 
 
