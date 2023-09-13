a02820e7180d61ffe0db11c3b4b4bc35d471875e
alvarogd16 <alvarogarcia16102000@gmail.com>
2023-09-13 18:50:28 +0200

    Arregla la identación y los espacios

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const {Server} = require("socket.io");
+const { Server } = require("socket.io");
 const io = new Server(server);
 const port = 3000;
 
@@ -12,40 +12,42 @@ const isPi = require("detect-rpi");
 /* Raspberry pi logic                         */
 /* ****************************************** */
 
-if(isPi()) {
-  console.log("Running in a Raspberry pi");
-   
-  const Gpio = require('pigpio').Gpio;
+if (isPi()) {
+	console.log("Running in a Raspberry pi");
+
+	const Gpio = require('pigpio').Gpio;
 
 	// Motor init
-  const motor = new Gpio(17, {mode: Gpio.OUTPUT});
-  const servoPulse = { 'RESET': 500, 'START': 2500 };
+	const motor = new Gpio(17, { mode: Gpio.OUTPUT });
+	const servoPulse = { 'RESET': 500, 'START': 2500 };
 
 	// Fin de carrera init
 	// Estan en orden de izquierda a derecha. Ej. pin4 para el más a la izquierda
-	const finCarreraConfig = { mode: Gpio.INPUT,
-															pullUpDown: Gpio.PUD_UP,
-															alert: true};
-	const finCarrera = [new Gpio(26, finCarreraConfig),
-											new Gpio(19, finCarreraConfig),
-											new Gpio(13, finCarreraConfig),
-											new Gpio(6, finCarreraConfig)];
+	const finCarreraConfig = {
+		mode: Gpio.INPUT,
+		pullUpDown: Gpio.PUD_UP,
+		alert: true
+	};
+	const finCarrera = [
+		new Gpio(26, finCarreraConfig),
+		new Gpio(19, finCarreraConfig),
+		new Gpio(13, finCarreraConfig),
+		new Gpio(6, finCarreraConfig)
+	];
 
 	// Para el debounce de los finales de carrera. El numero son los microsegundos que tiene que 
 	// estar el valor para que se notifique la interrupción con alert.
 	finCarrera.forEach(fC => fC.glitchFilter(10000));
 
-	
 	// Socket logic
-  io.on('connection', (socket) => {
-    console.log("Socket establish");
-    
-    socket.on("start", () => {
-      console.log("Race start. Servo down");
-			
+	io.on('connection', (socket) => {
+		console.log("Socket establish");
+
+		socket.on("start", () => {
+			console.log("Race start. Servo down");
+
 			motor.servoWrite(servoPulse.START);
 		});
-
 
 		socket.on("reset", () => {
 			console.log("Race reset. Servo up");
@@ -56,10 +58,10 @@ if(isPi()) {
 		// Crea los eventos para todos los finales de carrera
 		finCarrera.forEach((fC, index) => {
 			fC.on('alert', (level, tick) => {
-				if(level === 0) {
+				if (level === 0) {
 					console.log("Fin carrera " + index + " con nivel " + level);
 					// Sumo uno para que el indice empiece por 1
-					socket.emit("meta", (index+1));
+					socket.emit("meta", (index + 1));
 				}
 			})
 		});
@@ -67,7 +69,7 @@ if(isPi()) {
 	});
 
 } else {
-  console.log("Not running in a Raspberry pi");
+	console.log("Not running in a Raspberry pi");
 
 	// Para cuando se pruebe en un ordenador que no sea una raspberry
 	io.on('connection', (socket) => {
@@ -85,7 +87,6 @@ if(isPi()) {
 	});
 }
 
-
 /* ****************************************** */
 /* Web server                                 */
 /* ****************************************** */
@@ -95,10 +96,10 @@ app.use(express.static('public'));
 
 // Dont needed 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+	res.send('Hello World!')
 })
 
 // Start the server in the indicate port
 server.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+	console.log(`Example app listening at http://localhost:${port}`)
 })
